d184b7962724855c010472e4dff2fc05a70e9b98
Ferryykk <79855334+Ferryykk@users.noreply.github.com>
2021-04-05 23:34:14 +0700

    Add files via upload

---
+++ array_3.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Pemrograman PHP dengan Array</title>
+</head>
+<body>
+	<?php //perintah php
+	// Penulisan array dapat dibuat seperti berikut
+	$nama[] = "Ferry"; //membuat variabel $nama dengan array yang menyimpan string Ferry
+	$nama[] = "Kurniawan"; //membuat variabel $nama dengan array yang menyimpan string Kurniawan
+	$nama[] = "Ganteng"; //membuat variabel $nama dengan array yang menyimpan string Ganteng
+	echo $nama[1] . $nama[2] . $nama[0]; //menampilkan teks nilai variabel nama indeks ke 1, nilai variabel nama indeks ke 2, nilai variabel nama indeks ke 0 
+	echo "<br>"; //menampilkan ganti baris
+	?>
+
+</body>
+</html>
